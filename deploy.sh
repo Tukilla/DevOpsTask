1cc0d6f2bc6a5707b19158c60e630f3bc89050ff
Tukilla <146824386+Tukilla@users.noreply.github.com>
2023-10-03 17:52:39 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ docker stop pythonapp
 docker rm pythonapp
 
 # Run the Docker container, exposing it on port 8080
-docker run -d -p 8080:80 --name pythonapp tukilla/pythonapp
+docker run -d -p 8080:80 --name deployapp tukilla/pythonapp
 
