d658c67f80b010369ebac5801febb5f31866c4f6
Tukilla <turkalj.nikola84@gmail.com>
2023-10-03 14:38:28 +0000

    skripta

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pull the latest Docker image from Docker Hub
+docker pull tukilla/pythonapp:latest
+
+# Stop and remove any existing containers
+docker stop pythonapp
+docker rm pythonapp
+
+# Run the Docker container, exposing it on port 8080
+docker run -d -p 8080:80 --name pythonapp tukilla/pythonapp:latest
+
