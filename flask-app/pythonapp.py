853074143b460ec14aca5f10e1d6747a9b69a904
Tukilla <146824386+Tukilla@users.noreply.github.com>
2023-10-07 13:11:56 +0200

    Update pythonapp.py

--- flask-app/pythonapp.py
+++ flask-app/pythonapp.py
@@ -5,22 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return ' _nnnn_                      
-        dGGGGMMb     ,"""""""""""""".
-       @p~qp~~qMb    | Hello, DevOps! |
-       M|@||@) M|   _;..............'
-       @,----.JM| -'
-      JS^\__/  qKL
-     dZP        qKRb
-    dZP          qKKb
-   fZP            SMMb
-   HZM            MMMM
-   FqM            MMMM
- __| ".        |\dS"qML
- |    `.       | `' \Zq
-_)      \.___.,|     .'
-\____   )MMMMMM|   .'
-     `-'       `--' hjm'
+    return 'Hello, DevOps!'
 
 # main driver function
 if __name__ == "__main__":
