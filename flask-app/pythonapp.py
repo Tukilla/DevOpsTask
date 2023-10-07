afa0cd7f9a4004ba449cfbbcd3c798ea55d24a05
Tukilla <146824386+Tukilla@users.noreply.github.com>
2023-10-07 15:52:58 +0200

    Update pythonapp.py

--- flask-app/pythonapp.py
+++ flask-app/pythonapp.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, DevOps!'
+    return 'Hello, DevOps, another test deploy!'
 
 # main driver function
 if __name__ == "__main__":
